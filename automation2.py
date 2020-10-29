ae64eb479d921144da6e43fe49f86ca213d6568e
triplejblog <triplej03@naver.com>
2020-10-29 01:51:13 +0000

    train hourly

--- automation2.py
+++ automation2.py
@@ -10,11 +10,31 @@ class MyDriver(Driver):
         chrome_driver = "/home/kimjadong2020/Documents/chromedriver"
         self.driver = webdriver.Chrome(chrome_driver, options=chrome_options)
 
+    def department(self):
+        driver = self.driver
+        driver.find_element_by_xpath("//div[@id='quests_wrap']/div[5]").click()
+        driver.implicitly_wait(5)
+        for i in range(10):
+            driver.find_element_by_xpath(
+                    "//div[@id='region_scroll']/div/div/div[17]/div[3]/div/div/div[3]").click()
+        driver.find_element_by_xpath("//div[@id='header_slide_inner']/div[4]/div").click()
+        driver.find_element_by_id("slide_close").click()
+
+    def trainHourly(self):
+        # war train
+        self.getURL("https://rivalregions.com/#war")
+        try:
+            self.driver.find_element_by_xpath(
+                    "//div[@id='content']/div[4]/div[2]/div").click()
+            self.driver.find_element_by_id("war_my_alpha").click()
+        except:
+            print("train error")
+
 d = MyDriver()
-d.driver.implicitly_wait(2)
 d.getURL("https://rivalregions.com/#overview")
 
 for i in range(24):
+    d.trainHourly()
     for j in range(6):
         d.getURL("https://rivalregions.com/#overview")
         d.educationUp()
